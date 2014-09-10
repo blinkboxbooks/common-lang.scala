488daa61bc8617f34e2faad701c3c1e4aa767e1d
Greg Beech <greg@blinkbox.com>
2014-09-10 13:02:19 +0100

    Removed time helper methods

--- src/test/scala/com/blinkbox/books/time/ClockSpec.scala
+++ src/test/scala/com/blinkbox/books/time/ClockSpec.scala
@@ -1,6 +1,6 @@
 package com.blinkbox.books.time
 
-import org.joda.time.{Duration, DateTimeZone, DateTime}
+import org.joda.time.{DateTime, DateTimeZone}
 import org.scalatest.FlatSpec
 
 class ClockSpec extends FlatSpec {
@@ -8,7 +8,8 @@ class ClockSpec extends FlatSpec {
   "SystemClock" can "provide the current system time in UTC" in {
     val now = DateTime.now(DateTimeZone.UTC)
     val clockNow = SystemClock.now()
-    assert(clockNow >= now && clockNow <= now + Duration.millis(100))
+    assert(clockNow.getZone == DateTimeZone.UTC)
+    assert(!clockNow.isBefore(now) && !clockNow.isAfter(now.plusMillis(100)))
   }
 
   "StoppedClock" can "provide the time it was stopped at" in {
